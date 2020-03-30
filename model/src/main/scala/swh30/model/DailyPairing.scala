a1b5d42fb82f1037c77a10770a44b544c78f5abe
Stephen Whalley <stephen.whalley@sky.uk>
2020-03-30 23:54:22 +0100

    Make case classes final

---
+++ model/src/main/scala/swh30/model/DailyPairing.scala
@@ -0,0 +1,3 @@
+package swh30.model
+
+final case class DailyPairing(pairs: List[Pair]) extends Model
--- model/src/main/scala/swh30/model/History.scala
+++ model/src/main/scala/swh30/model/History.scala
@@ -1,5 +1,3 @@
 package swh30.model
 
-case class History(pairHistory: List[DailyPairing]) extends Model
-
-case class DailyPairing(pairs: List[Pair]) extends Model
+final case class History(pairHistory: List[DailyPairing]) extends Model
